86409edc3ebfccd4e16721f74886d8f96a97ae78
Hamsell Jimenez <hamsjimenez@gmail.com>
2026-08-19 12:50:59 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css'
+
+function TodoList(){
+const todos = [
+  {id: 1, text: "Learn JSX"},
+  {id: 2, text: "Understand props"},
+  {id: 3, text: "Master state & events"}
+];
+
+return (
+  <div>
+    <h2>My React Todos</h2>
+    <ul>
+      {todos,map((todo)=> (
+        <li>{todo.text}</li>
+      ))}
+    </ul>
+  </div>
+)
+}
+
+function App() {
+  
+  return (
+    <div>
+      <TodoList/>
+    </div>
+  );
+}
+
+
+// COMPONENT = Function that returns JSX
+
+export default App
